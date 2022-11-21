97190e3ba55b4446dfed8a32d60afe62cb2a4811
Tim Potter <tpot@frungy.org>
2022-11-22 06:40:08 +1100

    Install and configure Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './themes/**/layouts/**/*.html',
+    './content/**/layouts/**/*.html',
+    './layouts/**/*.html',
+    './content/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+  ],
+}
